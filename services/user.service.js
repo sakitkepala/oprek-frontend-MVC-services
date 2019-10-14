034982d21ab99e8ba5ed97ab0bcf6f89099d81db
sakitkepala <andikapriyotama@gmail.com>
2019-10-14 14:02:04 +0700

    [ADD] model & service

---
+++ services/user.service.js
@@ -0,0 +1,54 @@
+/**
+ * @class Service
+ * 
+ * Kelola data aplikasi
+ */
+
+class Service {
+  constructor() {
+    const penggunas = JSON.parse(localStorage.getItem('penggunas')) || [];
+    this.penggunas = penggunas.map(pengguna => new User(pengguna));
+  }
+
+  add(pengguna) {
+    this.penggunas.push(new User(pengguna));
+
+    this._commit(this.penggunas);
+  }
+
+  edit(id, penggunaYangDiedit) {
+    this.penggunas = this.penggunas.map(pengguna =>
+      pengguna.id === id
+        ? new User({
+            ...pengguna,
+            ...penggunaYangDiedit
+          })
+        : pengguna
+    );
+
+    this._commit(this.penggunas);
+  }
+
+  delete(_id) {
+    this.penggunas = this.penggunas.filter(({ id }) => id !== _id);
+
+    this._commit(this.penggunas);
+  }
+
+  toggle(_id) {
+    this.penggunas = this.penggunas.map(pengguna =>
+      pengguna.id === _id ? new User({ ...pengguna, komplet: !user.komplet }) : pengguna
+    );
+
+    this._commit(this.penggunas);
+  }
+
+  bindListPenggunaBerubah(callback) {
+    this.onListPenggunaBerubah = callback;
+  }
+
+  _commit(penggunas) {
+    this.onListPenggunaBerubah(penggunas);
+    localStorage.setItem('penggunas', JSON.stringify(penggunas));
+  }
+}
